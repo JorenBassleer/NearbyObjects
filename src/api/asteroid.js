683fa41d79f60de02597576a9d461744daf25314
Joren Bassleer <jorenbassleer1997@gmail.com>
2024-03-22 21:04:03 +0100

    fixed fetch

--- src/api/asteroid.js
+++ src/api/asteroid.js
@@ -1,15 +1,10 @@
-import axiosInstance from '@axios';
+import axios from 'axios';
 
 export const fetchLast7Days = async () => {
   try {
-    const response = await axiosInstance.get({
-      parameters: {
-        start_date: '2015-09-07',
-        end_date: '2015-09-08',
-      },
-    });
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}?start_date=2015-09-07&end_date=2015-09-08&api_key=${import.meta.env.VITE_API_KEY}`);
     // eslint-disable-next-line no-console
-    console.log('resposne:', response);
+    console.log('resposne:', response.data);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
