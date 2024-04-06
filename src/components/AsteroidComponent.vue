0838475ac597d715a11545dd8678818a28889b4b
Joren Bassleer <jorenbassleer1997@gmail.com>
2024-04-06 22:50:27 +0200

    reverted to earlier version

--- src/components/AsteroidComponent.vue
+++ src/components/AsteroidComponent.vue
@@ -7,14 +7,15 @@
       receive-shadow
       :scale="!isFocused ? asteroid?.estimated_diameter?.kilometers?.estimated_diameter_min / 100 : 0.001"
       draco
+      @click="$emit('click')"
     />
   </Suspense>
+  <!-- Maybe make component out of this -->
   <InformationPanel
     v-if="isFocused"
-    :position="[asteroidLocation.x, asteroidLocation.y, asteroidLocation.z]"
+    :position="[astroidLocation.x, astroidLocation.y, astroidLocation.z]"
   >
     <div class="flex justify-between items-center">
-      {{ asteroidLocation }}
       <h1 class="font-bold">
         ☄️ {{ asteroid.name }}
       </h1>
@@ -42,7 +43,6 @@ import {
 import { useRenderLoop } from '@tresjs/core';
 // eslint-disable-next-line import/no-unresolved
 import { GLTFModel } from '@tresjs/cientos';
-import { gsap } from 'gsap';
 import InformationPanel from './InformationPanel.vue';
 import AsteroidExtraInfo from './AsteroidExtraInfo.vue';
 
@@ -67,23 +67,22 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['update:component', 'onUnfocus']);
+const emit = defineEmits(['click', 'update:component', 'onUnfocus']);
 
 const { onLoop } = useRenderLoop();
 
 const astroidRef = shallowRef(null);
-const asteroidLocation = shallowRef({
+const astroidLocation = shallowRef({
   x: 0,
   y: 0,
   z: 0,
 });
 const showMoreInfo = shallowRef(false);
-const speedMultiplier = shallowRef(1);
 
 watch(astroidRef, (model) => {
   emit('update:component', model.value);
   onLoop(({ delta, elapsed }) => {
-    if (model.value) {
+    if (model.value && !props.isFocused) {
       // Get from asteroid data
       const orbitRadiusX = 3;
       // Get from asteroid data
@@ -91,23 +90,21 @@ watch(astroidRef, (model) => {
       const orbitSpeed = props.asteroid.close_approach_data[0].relative_velocity.kilometers_per_second / 10;
       // Make a composable out of this calculation
       const angle = (props.rotationEarth + elapsed) * orbitSpeed;
-      // const speedAdjustment = delta * orbitSpeed * speedMultiplier.value;
       /* eslint-disable no-param-reassign */
       model.value.rotation.y += Math.sin(delta * orbitSpeed);
       model.value.rotation.z += Math.sin(delta * orbitSpeed);
-      model.value.position.x = props.positionEarth.x + props.asteroid.close_approach_data[0].miss_distance.lunar * Math.sin(angle) * speedMultiplier.value;
-      model.value.position.z = props.positionEarth.z + props.asteroid.close_approach_data[0].miss_distance.lunar * Math.cos(angle) * speedMultiplier.value;
-
-      asteroidLocation.value = model.value.position;
+      model.value.position.x = props.positionEarth.x + props.asteroid.close_approach_data[0].miss_distance.lunar * Math.sin(angle);
+      model.value.position.z = props.positionEarth.z + props.asteroid.close_approach_data[0].miss_distance.lunar * Math.cos(angle);
+      astroidLocation.value = model.value.position;
+      /* eslint-enable no-param-reassign */
     }
   });
 });
 
-watch(() => props.isFocused, (newVal) => {
-  if (newVal) {
-    gsap.to(speedMultiplier, { value: 0, duration: 2 });
-  } else {
-    gsap.to(speedMultiplier, { value: 1, duration: 2 });
-  }
-});
+watch(
+  () => props.isFocused,
+  (v) => {
+    // console.log('isFocused update', v);
+  },
+);
 </script>
