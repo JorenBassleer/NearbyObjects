7e146329cf276e5524cf371288735789e1c06a15
Joren Bassleer <jorenbassleer1997@gmail.com>
2024-12-07 17:50:39 +0100

    stash

---
+++ src/components/TitleScreen.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="w-full h-full place-content-center">
+    Nearby objects
+    <div class="rounded p-4 text-center border border-white">
+      Enter
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
